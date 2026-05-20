14046cd63ad7c05450d351697cdafc20e289f336
Ade Satya Wahana <58018348+adesatya@users.noreply.github.com>
2026-05-20 13:47:46 +0000

    update v0.01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import pandas as pd
+import pickle
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Dashboard Analitik DJPb")
+
+# Sidebar Filter
+st.sidebar.header("Menu Filter")
+opsi = st.sidebar.selectbox("Pilih Model:", ["Klasifikasi", "Regresi"])
+
+# Load Model & Data
+model = pickle.load(open("models/model.pkl", "rb"))
+data = pd.read_csv("data/dataset.csv")
+st.dataframe(data.head())
+
+# Prediksi
+if st.button("Jalankan Prediksi"):
+    st.success("Prediksi berhasil!")
+    st.write(model.predict(data))
